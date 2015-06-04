21acef58eed6ea4c5b01c72606fc4973ff81ef6b
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-06-04 11:24:41 -0300

    arreglos menores.

--- public/livereload.js
+++ public/livereload.js
@@ -1,11 +1,17 @@
 var is_nodewebkit = (typeof process == "object");
+var DELAY = 2000;
+var refresh = false;
 
 if (is_nodewebkit) {
   var fs = require('fs');
 
   fs.watchFile('index.html', function() {
-    setTimeout(function() {
-      require('nw.gui').Window.get().reloadIgnoringCache();
-    }, 1000);
+    if (refresh === false) {
+      refresh = true;
+      console.log("Detectando cambio en el archivo index.html, actualizando ...");
+      setTimeout(function() {
+        require('nw.gui').Window.get().reloadIgnoringCache();
+      }, DELAY);
+    }
   });
 }
